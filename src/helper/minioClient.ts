9248ebddf34802aaa01380eb7e1ba739bc60b568
Arafie2603 <arrafiesetiawan@gmail.com>
2025-02-03 22:16:27 +0700

    chore(minio): update minio client config

--- src/helper/minioClient.ts
+++ src/helper/minioClient.ts
@@ -1,10 +1,10 @@
 import { Client } from 'minio';
 
-export const MINIO_ENDPOINT = 'localhost';  // or your actual MinIO server address
-export const MINIO_PORT = 9000;  // Default MinIO API port
-const MINIO_USE_SSL = false;
-const MINIO_ACCESS_KEY = process.env.MINIO_ACCESS_KEY;  // Replace with your access key
-const MINIO_SECRET_KEY = process.env.MINIO_SECRET_KEY;  // Replace with your secret key
+export const MINIO_ENDPOINT = process.env.MINIO_ENDPOINT || '';
+export const MINIO_PORT = parseInt(process.env.MINIO_PORT || '443');
+const MINIO_USE_SSL = true;
+const MINIO_ACCESS_KEY = process.env.MINIO_ACCESS_KEY || '';
+const MINIO_SECRET_KEY = process.env.MINIO_SECRET_KEY || '';
 
 export const minioClient = new Client({
     endPoint: MINIO_ENDPOINT,
